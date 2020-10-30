664d2a2a10b6ca6da122392bc56dbf29558f01c8
William Donovan-Seid <wdstwo@live.com>
2020-10-30 09:27:11 -0700

    add more seeded data to test 'popular' and 'random' endpoints

---
+++ AnimalShelterApi/Migrations/20201030161306_Initial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    CatId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CatName = table.Column<string>(maxLength: 20, nullable: false),
+                    CatBreed = table.Column<string>(nullable: false),
+                    CatAge = table.Column<int>(nullable: false),
+                    CatDetails = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.CatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    DogId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    DogName = table.Column<string>(maxLength: 20, nullable: false),
+                    DogBreed = table.Column<string>(nullable: false),
+                    DogAge = table.Column<int>(nullable: false),
+                    DogDetails = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.DogId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "CatAge", "CatBreed", "CatDetails", "CatName" },
+                values: new object[] { 1, 3, "Persian", "Likes knocking things over", "Snowball" });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "DogAge", "DogBreed", "DogDetails", "DogName" },
+                values: new object[] { 1, 2, "Pitbull", "Chases own shadow", "Sam" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cats");
+
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -16,12 +16,17 @@ namespace AnimalShelterApi.Models
     {
       builder.Entity<Cat>()
           .HasData(
-              new Cat { CatId = 1, CatName = "Snowball", CatBreed = "Persian", CatAge= 3, CatDetails = "Likes knocking things over"}
+              new Cat { CatId = 1, CatName = "Snowball", CatBreed = "Persian", CatAge= 3, CatDetails = "Likes knocking things over"},
+              new Cat { CatId = 2, CatName = "Baseball", CatBreed = "American", CatAge= 6, CatDetails = "Really Likes knocking things over"},
+              new Cat { CatId = 3, CatName = "Football", CatBreed = "Persian", CatAge= 9, CatDetails = "Super Likes knocking things over"}
+
           );
 
       builder.Entity<Dog>()
           .HasData(
-              new Dog { DogId = 1, DogName = "Sam", DogBreed = "Pitbull", DogAge= 2, DogDetails = "Chases own shadow"}
+              new Dog { DogId = 1, DogName = "Sam", DogBreed = "Pitbull", DogAge= 2, DogDetails = "Chases own shadow"},
+              new Dog { DogId = 2, DogName = "Sammy", DogBreed = "Pitbull", DogAge= 1, DogDetails = "Chases own shadow sometimes"},
+              new Dog { DogId = 3, DogName = "Sammuel", DogBreed = "Pitbull", DogAge= 20, DogDetails = "Chases own shadow all the time"}
           );
     }
   }
